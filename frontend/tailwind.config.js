778e3eeca38faf8abbbed8a0108f4d9c0a9cfe4e
yeabkal21b <yeabkalkassahun21@gmail.com>
2025-08-13 12:15:02 +0300

    Create frontend/tailwind.config.js

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        background: '#1E1E1E',
+        blue: '#3498db',
+        green: '#27ae60',
+        gold: '#ffd700',
+        text: '#f5f6fa',
+        rowAlt: '#232323'
+      }
+    }
+  },
+  plugins: [],
+}
