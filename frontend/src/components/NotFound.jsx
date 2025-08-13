5ac4368c9df9765ab3507f53971c15937ac2f0a4
yeabkal21b <yeabkalkassahun21@gmail.com>
2025-08-13 12:27:09 +0300

    Create frontend/src/components/NotFound.jsx

---
+++ frontend/src/components/NotFound.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function NotFound() {
+  return (
+    <div className="text-center text-gold text-2xl mt-20">
+      404 – Page Not Found
+    </div>
+  );
+}
